a5afa3c80dd124c2af5554cf88fbd171e851e5f3
deepanshu2001 <d.deepanshusharma12@gmail.com>
2026-01-06 16:18:05 -0800

    Setting up the backnend structure

---
+++ src/test/java/com/example/CopilotBackend/CopilotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CopilotBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopilotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
